a612121b72e42a6b06711d1cd8c689fef4226e16
AnupamKumarGupta <anupamkumargupta24@gmail.com>
2026-01-14 18:29:05 +0530

    Week 1 Day 2 assignment

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from app.agent import run_agent
 
 if __name__ == "__main__":
-    goal = "What is the difference between ChatGpt and Agentic AI?"
+    goal = "What is the difference AI and Agentic AI?"
     run_agent(goal)

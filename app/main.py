3b128d286d48f69c4f08eea60ba9621946e27194
AnupamKumarGupta <anupamkumargupta24@gmail.com>
2026-01-14 14:54:25 +0530

    Week 1 Day 2 assignment

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from app.agent import run_agent
 
 if __name__ == "__main__":
-    goal = "What is LLM in simple word"
+    goal = "What is the difference between ChatGpt and Agentic AI?"
     run_agent(goal)

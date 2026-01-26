c9cebc59f47b029432a0d95247c16347afd3fb4f
AnupamKumarGupta <anupamkumargupta24@gmail.com>
2026-01-26 21:40:41 +0530

    Resolve merge conflicts

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from app.agent import run_agent
 
 if __name__ == "__main__":
+    goal = "Explain inflation in very simple terms"
+    run_agent(goal)
+    
     with open("notice.txt", "r") as file:
         notice_text = file.read()
 
     run_agent(notice_text)
-
